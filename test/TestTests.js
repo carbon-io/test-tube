0173646dc0d1f97f6b995ff6d5f11ba5d9c63f3b
Greg Banks <gbanks@gmail.com>
2016-09-22 15:03:38 -0700

    add functionality
    - add setup/teardown hooks to req/res specs
    - add parent link to child tests on _init of the parent
    - bind res/req spec to its test if it is a function

---
+++ test/TestTests.js
@@ -0,0 +1,34 @@
+var assert = require('assert')
+
+var EJSON = require('ejson')
+var o = require('atom').o(module).main
+
+/******************************************************************************
+ * TestTests
+ */
+module.exports = o({
+
+  /**********************************************************************
+   * _type
+   */
+  _type: '../lib/Test',
+
+  /**********************************************************************
+   * name
+   */
+  name: "TestTests",
+
+  /**********************************************************************
+   * tests
+   */
+  tests: [
+    o({
+      _type: '../lib/Test',
+      name: 'EJSONStringifyWithCircularReferenceTest',
+      description: 'Test EJSON#stringify works with circular parent reference',
+      doTest: function() {
+        assert.equal(EJSON.stringify(this), JSON.stringify(this))
+      }
+    })
+  ]
+})
