b907997f680d9b2424e0df1b9f3f0bbae6655f1d
Greg Banks <gbanks@gmail.com>
2017-03-16 16:34:21 -0700

    fix context.global and use __.main in root test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var o = require('@carbon-io/atom').o(module)
 var _o = require('@carbon-io/bond')._o(module)
-var __ = require('@carbon-io/fibers').__(module)
+var __ = require('@carbon-io/fibers').__(module).main
 var Test = require('../lib/Test')
 
 __(function() {
@@ -12,7 +12,7 @@ __(function() {
       _o('./OrderingTests'),
       _o('./HttpTestTests'),
       _o('./UtilTests'),
-      //_o('../docs/code-frags/test')
+      _o('../docs/code-frags/test')
     ]
   })
 })
